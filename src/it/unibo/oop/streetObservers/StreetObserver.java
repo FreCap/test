e3be368da04b0f90743e3fabf867ea36dd509040
Francesco Capponi <FreCap@users.noreply.github.com>
2014-12-15 23:14:34 +0100

    reimplemented all structures as MVC

---
+++ src/it/unibo/oop/streetObservers/StreetObserver.java
@@ -0,0 +1,35 @@
+package it.unibo.oop.streetObservers;
+
+public class StreetObserver implements IStreetObserver{
+	
+	// tollero che cambi id, ma non che cambi posizione
+	private int id;
+	private final Double LATITUDE;
+	private final Double LONGITUDE;
+	
+	public StreetObserver(int id, Double lat, Double lng) {
+		this.id = id;
+		this.LATITUDE = lat;
+		this.LONGITUDE= lng;
+	}
+	
+	@Override
+	public void setID(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public int getID() {
+		return this.id;
+	}
+	
+	@Override
+	public Double getLat() {
+		return LATITUDE;
+	}
+	
+	@Override
+	public Double getLng() {
+		return LONGITUDE;
+	}
+}
