70868e45562e989eb882546be83f52fc3ed46f96
Francesco Capponi <FreCap@users.noreply.github.com>
2015-01-21 16:56:52 +0100

    added documentation for IStreetObserverObserver

--- src/it/unibo/oop/smartercities/controller/IStreetObserverObserver.java
+++ src/it/unibo/oop/smartercities/controller/IStreetObserverObserver.java
@@ -3,11 +3,28 @@ package it.unibo.oop.smartercities.controller;
 import it.unibo.oop.smartercities.datatype.I.IInfoStreetObserver;
 import it.unibo.oop.smartercities.datatype.I.IStreetObserver;
 
-// questo observer ha il compito di chiedere al controller delle info relative ad uno StreetObserver,
-// quando viene richiesto dalla GUI
+
+/**
+ * Questa interfaccia funzionale ha il compito di astrarre la logica del sistema dalla
+ * parte di View, disaccoppiando la richiesta della View di ricevere delle informazioni 
+ * su di uno {@link IStreetObserver} dall'effettiva implementazione del Model.
+ * 
+ * @author Federico Bellini
+ *
+ */
 
 @FunctionalInterface
 public interface IStreetObserverObserver{
 	
+	/**
+	 * 
+	 * Restituisce un oggetto del tipo {@link IInfoStreetObserver} contenente le
+	 * informazioni sull'{@link IStreetObserver} richiesto.
+	 * 
+	 * @param streetObserver
+	 * 			L'{@link IStreetObserver} di cui si richiedono informazioni.
+	 * @return 
+	 * 			Un oggetto {@link IStreetObserver} con le informazioni richieste.
+	 */
 	IInfoStreetObserver getStreetObserverInfo(IStreetObserver streetObserver);
 }
