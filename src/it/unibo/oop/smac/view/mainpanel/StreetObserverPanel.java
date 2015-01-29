91f8349dbd2047c0c8289321ef97b5ab2645b8ad
Francesco Capponi <FreCap@users.noreply.github.com>
2015-01-29 12:48:04 +0100

    in main panel when you click a streetObserver it changes background color

--- src/it/unibo/oop/smac/view/mainpanel/StreetObserverPanel.java
+++ src/it/unibo/oop/smac/view/mainpanel/StreetObserverPanel.java
@@ -3,6 +3,7 @@ package it.unibo.oop.smac.view.mainpanel;
 import it.unibo.oop.smac.datatype.I.IStreetObserver;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.function.Consumer;
@@ -16,24 +17,26 @@ import javax.swing.border.TitledBorder;
 public class StreetObserverPanel extends JPanel{
 	
 	private static final long serialVersionUID = 984911465198419L;
-	private static final String DEFAULT_PATH = "/images/";
 	private static final int DELAY = 150;
 	private static final ImageIcon PASSAGE_ACTIVE_ICON = 
-			new ImageIcon(MainPanel.class.getResource(DEFAULT_PATH + "activeButton.png"));
+			new ImageIcon(MainPanel.class.getResource("/images/activeButton.png"));
 	private static final ImageIcon PASSAGE_INACTIVE_ICON = 
-			new ImageIcon(MainPanel.class.getResource(DEFAULT_PATH + "inactiveButton.png"));
+			new ImageIcon(MainPanel.class.getResource("/images/inactiveButton.png"));
 	
+	private final Color DEFAUTLT_COLOR_BACKGROUND;
 	private final JLabel mapLabel;
 	private final JLabel passageLabel;
 	
 	public StreetObserverPanel(IStreetObserver streetObserver, Consumer<IStreetObserver> consumer) {
+		super();
 		this.setLayout(new BorderLayout());
 		this.setBorder(new TitledBorder("Street Observer  " + streetObserver.getID()));
+		this.DEFAUTLT_COLOR_BACKGROUND = this.getBackground();
 		
 		this.mapLabel = new JLabel();
 		ImageIcon ii = OSMStaticMapConstructor.getInstance().getStaticMap(
-					streetObserver.getLatitude(),
-					streetObserver.getLongitude()
+				streetObserver.getLatitude(),
+				streetObserver.getLongitude()
 		);
 		
 		this.mapLabel.setIcon(ii);
@@ -45,33 +48,44 @@ public class StreetObserverPanel extends JPanel{
 		this.add(passageLabel, BorderLayout.EAST);
 		
 		this.addMouseListener(new MouseListener(){
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				consumer.accept(streetObserver);
-			}
-			@Override
-			public void mouseEntered(MouseEvent arg0) {
-			}
-			@Override
-			public void mouseExited(MouseEvent arg0) {
-			}
-			@Override
-			public void mousePressed(MouseEvent arg0) {
-			}
-			@Override
-			public void mouseReleased(MouseEvent arg0) {
-			}
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					consumer.accept(streetObserver);
+				}
+				@Override
+				public void mouseEntered(MouseEvent arg0) {
+					setColor(Color.YELLOW);
+				}
+				@Override
+				public void mouseExited(MouseEvent arg0) {
+					setColor(DEFAUTLT_COLOR_BACKGROUND);
+				}
+				@Override
+				public void mousePressed(MouseEvent arg0) {
+					setColor(Color.GREEN);
+				}
+				@Override
+				public void mouseReleased(MouseEvent arg0) {
+					setColor(Color.YELLOW);
+				}
 		});
 	}
 	
+	private void setColor(Color c){
+		try {
+			SwingUtilities.invokeLater(() -> this.setBackground(c));
+		} catch (Exception e) {
+			// do nothing
+		}
+	}
+	
 	public void displayPassage() {
-		SwingUtilities.invokeLater(() -> { setButtonOn(); });
+		SwingUtilities.invokeLater(() -> { this.setButtonOn(); });
 		try {
 			Thread.sleep(DELAY);
 		} catch(InterruptedException e) {
 		}
-		
-		SwingUtilities.invokeLater(() -> { setButtonOff(); });
+		SwingUtilities.invokeLater(() -> { this.setButtonOff(); });
 	}
 	
 	private void setButtonOn() {
